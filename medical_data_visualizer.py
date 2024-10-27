887d473a7e56d504c23eb5dd14af1fd52af26a4b
Péter Gönczi <gnczipeter@gmail.com>
2024-10-27 17:59:47 +0000

    modified:   medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,18 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv", header = 0)
 
 # 2
 df['overweight'] = None
 
 # 3
+df["cholesterol"] = df["cholesterol"].astype(int)
+df["gluc"] = df["gluc"].astype(int)
 
+
+#data wrangling for gluc
+df.loc[df["gluc"]==1, "gluc"] = 0  
+df.loc[df["gluc"]> 1, "gluc"] = 1 
+
+#data wrangling for cholesterol , experimenting with mapping
+df["cholesterol"] = df['cholesterol'].map( lambda x:1 if x>1 else 0 )
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = sns.catplot( data= df, x= "sex", y= "height")
 
 
     # 6
